23cfb2fd73cd5e383591df0c2ab954609f0e2817
komxun <komxun@gmail.com>
2023-10-19 10:59:51 +0100

    update codes names

--- main_sender.m
+++ main_sender.m
@@ -1,6 +1,7 @@
 clc, clear, close all
 load("leader_trajectory.mat")
 
+%Open udp port
 port = 51002;
 coms = udpport("datagram","IPV4","LocalPort",port);
 broadcast_address = "127.0.0.255";
@@ -11,17 +12,13 @@ broadcast_address = "127.0.0.255";
 % procData2 = procData/10;
 procTraj = lead_traj.*10;
 for j = 1:length(lead_traj)
-    %Open udp port
     
-    
-    
-    % while true
     %Add paths
     command.header = 'STR';
 %     command.msg_id = uint8(2);
 %     command.msg_type = uint8(0);
 
-    command.array = uint16([procTraj(:,j)' 0 0 0]);
+    command.array = int16([procTraj(:,j)' 0 0 0]);
 %     command.array = int16(procData);
 
     command.terminator='END';
